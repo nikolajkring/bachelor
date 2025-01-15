12c33a0958f731baff36c3a46aa6a4c5ee20071d
Nikolaj Kring <nikolajkring@live.dk>
2025-01-15 17:06:30 +0100

    Change kitchen details

--- resources/views/kitchen-details.blade.php
+++ resources/views/kitchen-details.blade.php
@@ -26,75 +26,76 @@
                     <div id="items-container">
                         @include('partials.items', ['items' => $items])
                     </div>
-                    
-                    {{-- wanna add more items? --}}
-                    <div class="py-12">
-                        <h2 class="text-2xl font-bold mb-2">Add more items {{ $kitchen->name }}</h2>
-                        <form 
-                            method="POST" 
-                            action="{{ route('items.store') }}" 
-                            hx-post="{{ route('items.store') }}" 
-                            hx-target="#items-container" 
-                            hx-swap="innerHTML">
-                            @csrf
-                            <input type="hidden" name="kitchen_id" value="{{ $kitchen->id }}">
-                            <div class="flex justify-between border rounded py-2 px-3">
-                                <label for="name">Item name:</label>
-                                <input type="text" name="name" id="name" required>
-                            </div>
-                            <div class="flex justify-between border rounded py-2 px-3">
-                                <label for="price">Price:</label>
-                                <input type="number" name="price" id="price" required>
-                            </div>
-                            <div class="flex justify-between border rounded py-2 px-3">
-                                <label for="amount">Amount:</label>
-                                <input type="number" name="amount" id="amount" required>
-                            </div>
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 border font-bold py-2 px-4 rounded">Add item</button>
-                    </form>
-                    </div>  
                 </div>
+            </div>
+        
 
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6 p-6">
+                {{-- wanna add more items? --}}
+                <div class="py-12">
+                    <h2 class="text-2xl font-bold mb-2">Add more items {{ $kitchen->name }}</h2>
+                    <form 
+                        method="POST" 
+                        action="{{ route('items.store') }}" 
+                        hx-post="{{ route('items.store') }}" 
+                        hx-target="#items-container" 
+                        hx-swap="innerHTML">
+                        @csrf
+                        <input type="hidden" name="kitchen_id" value="{{ $kitchen->id }}">
+                        <div class="flex justify-between border rounded py-2 px-3">
+                            <label for="name">Item name:</label>
+                            <input type="text" name="name" id="name" required>
+                        </div>
+                        <div class="flex justify-between border rounded py-2 px-3">
+                            <label for="price">Price:</label>
+                            <input type="number" name="price" id="price" required>
+                        </div>
+                        <div class="flex justify-between border rounded py-2 px-3">
+                            <label for="amount">Amount:</label>
+                            <input type="number" name="amount" id="amount" required>
+                        </div>
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 border font-bold py-2 px-4 rounded">Add item</button>
+                    </form>
+                </div>  
+            </div>
+            
+
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6 p-6">
                 {{-- Show your transactions --}}
-                <div class="py-6 p-6">
+                <div class="py-6">
                     <a href="#" class="bg-blue-500 hover:bg-blue-700 border font-bold py-2 px-4 rounded" 
                     hx-get="/transactions/{{ $kitchen->id }}/{{ Auth::user()->id }}" 
                     hx-target="#dashboard_container">Your kitchen transactions</a>
                 </div>
 
-                    {{-- wanna delete the kitchen? --}}
-                    {{-- if owner show this --}}
-                    {{-- @if (Auth::user()->id === $is_owner->user_id && $user_kitchen->is_owner === 1 && $user_kitchen->kitchen_id === $kitchen->id) --}}
-                        <div class="py-6 p-6">
-                            <form 
-                                method="POST" 
-                                action="{{ route('kitchen.destroy', ['id' => $kitchen->id]) }}" 
-                                hx-post="{{ route('kitchen.destroy', ['id' => $kitchen->id]) }}"
-                                onsubmit="return confirm('Are you sure you want to delete this kitchen?');"
-                                >
-                                @csrf
-                                @method('DELETE')
-                                {{-- go to dashboard --}}
-                                <button type="submit" class="bg-red-500 hover:bg-red-700 border font-bold py-2 px-4 rounded">Delete kitchen</button>
-                            </form>
-                        </div>
+                {{-- wanna delete the kitchen? --}}
+                <div class="py-6">
+                    <form 
+                        method="POST" 
+                        action="{{ route('kitchen.destroy', ['id' => $kitchen->id]) }}" 
+                        hx-post="{{ route('kitchen.destroy', ['id' => $kitchen->id]) }}"
+                        onsubmit="return confirm('Are you sure you want to delete this kitchen?');"
+                        >
+                        @csrf
+                        @method('DELETE')
+                        {{-- go to dashboard --}}
+                        <button type="submit" class="bg-red-500 hover:bg-red-700 border font-bold py-2 px-4 rounded">Delete kitchen</button>
+                    </form>
+                </div>
 
-                        {{-- go to transactions-details.blade.php with htmx --}}
-                        <div class="py-6 p-6">
-                            <a href="#" class="bg-blue-500 hover:bg-blue-700 border font-bold py-2 px-4 rounded" 
-                            hx-get="/transactions/{{ $kitchen->id }}" 
-                            hx-target="#dashboard_container">Kitchen transactions</a>
-                        </div>
+                {{-- go to transactions-details.blade.php with htmx --}}
+                <div class="py-6">
+                    <a href="#" class="bg-blue-500 hover:bg-blue-700 border font-bold py-2 px-4 rounded" 
+                    hx-get="/transactions/{{ $kitchen->id }}" 
+                    hx-target="#dashboard_container">Kitchen transactions</a>
+                </div>
 
-                        {{-- go to settle_account --}}
-                        <div class="py-6 p-6">
-                            <a href="#" class="bg-blue-500 hover:bg-blue-700 border font-bold py-2 px-4 rounded" 
-                            hx-get="/settlements/{{ $kitchen->id }}" 
-                            hx-target="#dashboard_container">Settle account</a>
-                        </div>
-                    {{-- @endif --}}
-                
-            
+                {{-- go to settle_account --}}
+                <div class="py-6">
+                    <a href="#" class="bg-blue-500 hover:bg-blue-700 border font-bold py-2 px-4 rounded" 
+                    hx-get="/settlements/{{ $kitchen->id }}" 
+                    hx-target="#dashboard_container">Settle account</a>
+                </div>
             </div>
         </div>
     </div>
